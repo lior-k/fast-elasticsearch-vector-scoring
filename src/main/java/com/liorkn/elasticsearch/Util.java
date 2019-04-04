f688c3a5a74e63f7dfc4236054854d587e342f03
Iakov Gorelik <igoreli1@binghamton.edu>
2019-04-04 09:45:15 -0400

    Broke it back into multiple files for simplicity, added support for encodedVector field, added runAsLong(), changed searchVector to be a primitive type, made SCRIPT_SOURCE private static class member

--- src/main/java/com/liorkn/elasticsearch/Util.java
+++ src/main/java/com/liorkn/elasticsearch/Util.java
@@ -9,23 +9,22 @@ import java.util.Base64;
  */
 public class Util {
 
-    public static final double[] convertBase64ToArray(String base64Str) {
-        final byte[] decode = Base64.getDecoder().decode(base64Str.getBytes());
-        final DoubleBuffer doubleBuffer = ByteBuffer.wrap(decode).asDoubleBuffer();
+	public static double[] convertBase64ToArray(String base64Str) {
+	    final byte[] decode = Base64.getDecoder().decode(base64Str.getBytes());
+	    final DoubleBuffer doubleBuffer = ByteBuffer.wrap(decode).asDoubleBuffer();
+	    final double[] dims = new double[doubleBuffer.capacity()];
+	    doubleBuffer.get(dims);
+	    return dims;
+	}
 
-        final double[] dims = new double[doubleBuffer.capacity()];
-        doubleBuffer.get(dims);
-        return dims;
-    }
-
-    public static final String convertArrayToBase64(double[] array) {
-        final int capacity = 8 * array.length;
-        final ByteBuffer bb = ByteBuffer.allocate(capacity);
-        for (int i = 0; i < array.length; i++) {
-            bb.putDouble(array[i]);
-        }
-        bb.rewind();
-        final ByteBuffer encodedBB = Base64.getEncoder().encode(bb);
-        return new String(encodedBB.array());
-    }
+	public static String convertArrayToBase64(double[] array) {
+		final int capacity = Double.BYTES * array.length;
+		final ByteBuffer bb = ByteBuffer.allocate(capacity);
+		for (double v : array) {
+		    bb.putDouble((double) v);
+		}
+		bb.rewind();
+		final ByteBuffer encodedBB = Base64.getEncoder().encode(bb);
+		return new String(encodedBB.array());
+	}
 }
